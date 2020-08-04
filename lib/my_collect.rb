22547a7214178c51537aa979b34670fa569e7f90
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-08-04 04:27:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array)
   i = 0
   new_collection = []
-  while i < collection.length 
+  while i < collection.length
     new_collection << yeild (collection[i])
     i += 1
-  end 
+  end
   new_collection
 end
